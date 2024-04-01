411825175667285b39cc54cfe0efd26105c92520
Guldoman <giulio.lettieri@gmail.com>
2024-04-01 09:20:25 +0200

    Only return functions from `lpm.run`

--- src/lpm.lua
+++ src/lpm.lua
@@ -2178,7 +2178,7 @@ function lpm.run(ARGS)
     error("unknown command: " .. ARGS[2])
   elseif result == true and JSON then
     io.stdout:write(json.encode({ actions = actions, warnings = warnings }))
-  else
+  elseif type(result) == "function" then
     return result
   end
 end
